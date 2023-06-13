9ca7121ecf6811db17d847e55144fc748459b40e
nullbitx8 <nullbitx8@protonmail.com>
2023-06-13 18:27:51 +0100

    Search BNS (base name service) for a bns name, before searching ens.

--- packages/core/src/actions/ens/fetchEnsName.ts
+++ packages/core/src/actions/ens/fetchEnsName.ts
@@ -16,6 +16,12 @@ export async function fetchEnsName({
   address,
   chainId,
 }: FetchEnsNameArgs): Promise<FetchEnsNameResult> {
+  // look for BNS (base name service) first                                
+  const resp = await fetch(`https://testnet-api.basename.app/v1/web3-names/${address}`);
+  const data = await resp.json();
+  if (data.length > 0 && data[0]?.bns) return data[0]?.bns;
+
+  // look for ens if bns could not be found    
   const publicClient = getPublicClient({ chainId })
   return publicClient.getEnsName({
     address: getAddress(address),
